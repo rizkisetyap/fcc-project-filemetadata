bcc2659932edef60ed8d5c475dfa1a7c70af4ce4
rizkietyap <setyakiki76@gmail.com>
2022-02-03 14:16:26 +0700

    initial commit

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
 
 var app = express();
+var upload = multer({ dest: 'public/uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.send('File not provided');
+  }
 
-
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
